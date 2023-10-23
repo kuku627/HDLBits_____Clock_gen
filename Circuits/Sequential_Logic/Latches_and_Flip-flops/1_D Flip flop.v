df99b25b58069a67bd96319536d8107a53cf811d
kuku627 <dltjdcjf106@gmail.com>
2023-10-23 20:59:35 +0900

    Sequential_Logic

---
+++ Circuits/Sequential_Logic/Latches_and_Flip-flops/1_D Flip flop.v
@@ -0,0 +1,14 @@
+module top_module(
+    input clk,
+    input d,
+    output reg q
+);    // Use a clocked always block
+    //   copy d to q at every positive edge of clk
+    //   Clocked always blocks should use non-blocking assignments
+
+
+    always @(posedge clk) 
+        q<=d;
+    
+
+endmodule
