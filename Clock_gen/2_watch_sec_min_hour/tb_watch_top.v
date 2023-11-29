65dee9894a1b5250bfa231daba12f5784c112303
kuku627 <dltjdcjf106@gmail.com>
2023-11-29 10:19:39 +0900

    one_sec_gen.v

---
+++ Clock_gen/2_watch_sec_min_hour/tb_watch_top.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+module tb_watch_top;
+localparam P_COUNT_BIT = 30; // (default) 30b, under 1GHz. 2^30 = 1073741824
+localparam P_SEC_BIT	 = 6; // 2^6 = 64
+localparam P_MIN_BIT	 = 6; // 2^6 = 64 
+localparam P_HOUR_BIT = 5; // 2^5 = 32 
+
+reg clk;
+reg reset;
+reg i_run_en;
+reg [P_COUNT_BIT-1:0]	i_freq;
+wire [P_SEC_BIT-1:0]	o_sec;
+wire [P_MIN_BIT-1:0]	o_min;
+wire [P_HOUR_BIT-1:0]	o_hour;
+
+
+
+always #5 clk= ~clk;
+
+initial begin
+
+    i_freq      <=10;
+    reset 		<= 0;
+    clk     	<= 0;
+	i_run_en 	<= 0;    
+
+
+#100
+    reset       <=1;
+#10
+    reset       <=0;
+    i_run_en    <=1;
+
+# 1000000
+
+
+    i_run_en    <=0;
+end
+
+
+
+initial begin
+    $dumpfile("watch_top.vcd");
+    $dumpvars(0,tb_watch_top);
+
+end
+
+
+watch_top
+# (
+	.P_COUNT_BIT	(P_COUNT_BIT),
+	.P_SEC_BIT	 	(P_SEC_BIT	),
+	.P_MIN_BIT	 	(P_MIN_BIT	),
+	.P_HOUR_BIT		(P_HOUR_BIT	)
+) watch_top_inst(
+	.clk 				(clk),
+	.reset 				(reset),
+	.i_run_en			(i_run_en),
+	.i_freq				(i_freq),
+    .o_sec				(o_sec ),
+    .o_min				(o_min ),
+    .o_hour				(o_hour)
+);
+
+
+
+
+endmodule
